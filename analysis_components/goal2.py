236617985f26a2075c91d2f8220e15003116ea78
Jill Mao <jjmao@email.wm.edu>
2018-05-03 23:17:10 -0400

    newest goal2

--- analysis_components/goal2.py
+++ analysis_components/goal2.py
@@ -12,6 +12,9 @@ Created on Sun Apr 29 18:43:14 2018
 # using SSL, or using it incorrectly).
 ############################
 import re
+from androguard.misc import AnalyzeAPK
+from androguard.core.bytecodes.apk import APK
+from androguard.misc import clean_file_name
 
 '''
 The main starting point for this analysis. The objects needed to analyze
@@ -30,9 +33,6 @@ def main(apk, dvm, dx):
     count_https = 0
     
     flag1 = 0
-    flag2 = 0
-    flag3 = 0
-    flag4 = 0
     print("-- Analyzing SSL missuse --")
     
     # Example: Get a list of all the strings that start with "http(s)://"
@@ -46,7 +46,7 @@ def main(apk, dvm, dx):
         # A lot of these come from libraries
         if re.match('https?://', string, re.IGNORECASE):
             urls.append(string)
-    # Iterate through all classes
+   # Iterate through all classes
     for c in dvm.get_classes():
         # Skip classes not in this package
         if not (package in c.get_name()):
@@ -56,29 +56,21 @@ def main(apk, dvm, dx):
                 string = string.strip()
                 #Q5    
                 #check later if all managers and hostnames are being allowed
-                if re.search(r'allow', string, re.IGNORECASE):
+                if re.search('allow', string, re.IGNORECASE):
                     allow.append(string)
-                if re.search(r'trust', string, re.IGNORECASE):
+                if re.search('trust', string, re.IGNORECASE):
                     trust.append(string)
+
                 #Q6
                 #if a certificate is self signed, then generateCertificate() must be used to hardcode it
                 #step1: read in certificate
-                if re.search(r'generateCertificate', string, re.IGNORECASE):
-                    flag1 += 1
-                    # print("self signed certificate")
-                #step2: create custom trustmanager
-                #create keystore containing trusted CAs
-                if re.search(r'keystore', string, re.IGNORECASE) and flag1>0:
-                    flag2 += 1
-                #create trust manager that trusts the CAs in our keystore
-                if re.search(r'trustmanagerfactory', string, re.IGNORECASE) and flag2>0:
-                    flag3 += 1
-                #create sslcontext that uses our trustmanager
-                if re.search(r'sslcontext', string, re.IGNORECASE) and flag3>0:
-                    flag4 += 1
-    #at the end of the app, if the 4th flag is down, then the self-signed certificate is correctly pinned 
-    if flag1 > 0 and flag4 < 0:
-        result[0] = True
+                if re.search('certificate', string, re.IGNORECASE):
+                    flag1 +=1
+                if flag1>0 and re.search('okhttp', string, re.IGNORECASE):
+                    result[0]=True
+                if flag1>0 and re.search('validatepin', string, re.IGNORECASE):
+                    result[0] = True
+
         # print("self-signed certificate incorrectly pinned")
         
     # print(urls)
@@ -91,18 +83,16 @@ def main(apk, dvm, dx):
         result[1] = True
         # print('mix of http and https!')
             #if both are found, then the app is a mixture
-    
+
     for string in allow:
-        if re.search(r'all', string, re.IGNORECASE) and re.search(r'hostname', string, re.IGNORECASE):
+        if re.search('all', string, re.IGNORECASE) and re.search('hostname', string, re.IGNORECASE):
             # print('allows all host name verifiers!')
             result[2] = True
             #if some variation of the string allowallhostnames is found
-            
+
     for string in trust:
-        if re.search(r'all', string, re.IGNORECASE) and re.search(r'manager', string, re.IGNORECASE):
+        if re.search('all', string, re.IGNORECASE) and re.search('manager', string, re.IGNORECASE):
             # print('allows all trust managers!')
             result[3] = True
             #if some variation of the string allowalltrustmanagers is found
     return result
-       
-        
